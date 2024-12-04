08842bfe1809a3af30bd3b560d60e54a75ef4281
QuasimKhan <quasimkhanpersonal@gmail.com>
2024-12-04 14:07:35 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
